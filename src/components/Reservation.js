75285282227a98e49ea26e294a0d3d1423bddc9c
DoopTu <tuhandsome111@gmail.com>
2023-02-10 11:30:50 +0700

    11111

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Helmet from "react-helmet";
 import { HelmetProvider } from "react-helmet-async";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faTimes, faInfoCircle, faUser, faClock } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faTimes, faInfoCircle, faUser, faClock, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Slider from "./Slider"
 import Header from "./Header";
 import Footer from "./Footer";
@@ -12,12 +12,29 @@ const Reservation = () => {
         <div>
             <Header></Header>
             <Slider></Slider>
-            <div class="row col-lg-6 step-reservation">
-                <div class="col-lg-3 rounded-circle">sdfsdf</div>
-                <div class="col-lg-3 rounded-circle">sdfsdfsdf</div>
-                <div class="col-lg-3 rounded-circle">sdfsdfsdf</div>
+            <h2 style={{ textAlign: 'center', paddingTop: '30px', color: '#BA3925' }}>Processing...</h2>
+            <div class="step-reservation container d-flex align-items-center justify-content-center">
+                <div className="circle">
+                    <div class="col-lg-3 rounded-circle"><span>1</span></div>
+                    <h6 style={{display:'block', width:'80px', textAlign:'center', marginLeft:'10px'}}>Reservation Details</h6>
+                </div>
+                <FontAwesomeIcon style={{ fontSize: '25px' }} icon={faArrowRight}></FontAwesomeIcon>
+                <div className="circle">
+                    <div class="col-lg-3 rounded-circle"><span>1</span></div>
+                    <h6 style={{display:'block', width:'80px', textAlign:'center', marginLeft:'10px'}}>Reservation Details</h6>
+                </div>
+                <FontAwesomeIcon style={{ fontSize: '25px' }} icon={faArrowRight}></FontAwesomeIcon>
+                <div className="circle">
+                    <div class="col-lg-3 rounded-circle"><span>1</span></div>
+                    <h6 style={{display:'block', width:'80px', textAlign:'center', marginLeft:'10px'}}>Reservation Details</h6>
+                </div>
+                <FontAwesomeIcon style={{ fontSize: '25px' }} icon={faArrowRight}></FontAwesomeIcon>
+                <div className="circle">
+                    <div class="col-lg-3 rounded-circle"><span>1</span></div>
+                    <h6 style={{display:'block', width:'80px', textAlign:'center', marginLeft:'10px'}}>Reservation Details</h6>
+                </div>
             </div>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </div>
     )
 
