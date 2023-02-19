d7bd5a337785c8ca1548d589a1790fe1dc3874d2
Bekhongbikcode <tammtse161087@fpt.edu.vn>
2023-02-19 14:16:54 +0700

    update user page

---
+++ src/components/cancelledBooking.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { Link } from "react-router-dom";
+import AccountBody from './accountBody';
+import AccountFooter from './accountFooter';
+import AccountHeader from './accountHeader';
+
+
+function Cancelled() {
+    return (
+        <div className=''>
+            <AccountHeader />
+            <div className='Past_Choice'>
+                <Link className='Past_Link' to={'/Past'}><a>All</a></Link>
+                <br></br><br></br>
+                <Link className='Past_Link' to={'/Completed'}><a>Completed</a></Link>
+                <br></br><br></br>
+                <Link className='Past_Link' to={'/Cancelled'}><a>Cancelled</a></Link>
+            </div>
+            <AccountBody />
+            <AccountFooter />
+        </div>
+    );
+}
+
+export default Cancelled;
